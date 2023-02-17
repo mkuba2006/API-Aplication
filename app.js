4966374e4655c1f8f1bb41c678dc7e86271e31fe
Jakub Myrta <98383163+mkuba2006@users.noreply.github.com>
2023-02-17 22:58:49 +0100

    good job

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const BodyParser = require('body-parser');
 const request = require('request');
 const app = express();
-
+const https = require('https');
+const { response } = require('express');
+const { parse } = require('path');
 
 app.use(express.static('public'));
 app.use(BodyParser.urlencoded({extended: true}));
@@ -12,12 +14,51 @@ app.get('/', function(req, res){
 })
 
 app.post("/", function(req, res){
- const firstName = req.body.FName;
- const lastName = req.body.LName;
+ const firstName = req.body.fName;
+ const lastName = req.body.lName;
  const email = req.body.Email;
+ const data = {
+    members: [
+    {
+        email_adress: email,
+        status: "subscribed",
+        merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+        }
+    }
+    ]
+};
+ const jsonData= JSON.stringify(data);
+ const url = 'https://us21.api.mailchimp.com/3.0/lists/f872f228b5';
+ const options = {
+     method: 'POST',
+     auth: 'mkuba2006:85df511036059b0e219b734e18b86511-us21',
+ };
+ const request = https.request(url, options, function(resp){
+    if(resp.statusCode === 200){
+        res.sendFile(__dirname + '/success.html')
+    }else{
+        res.sendFile(__dirname + '/fail.html')
+    }
+    resp.on('data', function(data){
+        console.log(JSON.parse(data));
+    })
+ })
+ 
+ request.write(jsonData);
+ request.end();
  console.log(firstName, lastName, email);
+})
+
+app.post('/fail', function(req, res){
+ res.redirect('/');
 })
 
 app.listen(3000, function(){
     console.log('Started 3000');
-})
+}) 
+
+
+//85df511036059b0e219b734e18b86511-us21
+//audience f872f228b5
